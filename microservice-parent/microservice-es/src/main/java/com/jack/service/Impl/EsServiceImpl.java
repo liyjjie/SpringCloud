2f4972acc13dc42a98a87f727f196f135bcef458
liyjjie <liyongjie@tinsecret.com>
2020-11-25 10:16:10 +0800

    删除多余导入包

--- microservice-parent/microservice-es/src/main/java/com/jack/service/Impl/EsServiceImpl.java
+++ microservice-parent/microservice-es/src/main/java/com/jack/service/Impl/EsServiceImpl.java
@@ -1,10 +1,7 @@
 package com.jack.service.Impl;
 
-import com.carrotsearch.hppc.LongArrayList;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.google.gson.Gson;
 import com.jack.conf.EsConf;
 import com.jack.dao.EsDao;
@@ -18,20 +15,13 @@ import com.jack.vo.EsInsertReturn;
 import com.jack.vo.EsInsertVo;
 import com.jack.vo.EsUpdateVo;
 import com.jack.vo.UserVo;
-import net.sf.json.JSONObject;
-import org.apache.commons.collections.ArrayStack;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.util.StringUtil;
-import org.aspectj.lang.annotation.After;
-import org.elasticsearch.common.recycler.Recycler;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
