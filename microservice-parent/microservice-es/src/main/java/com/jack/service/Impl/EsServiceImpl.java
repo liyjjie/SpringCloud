da426ca0a6f43f7116a31e5643d8c1f914f7f406
liyjjie <liyongjie@tinsecret.com>
2020-11-25 13:19:17 +0800

    动态条件查询爬楼梯的方式

--- microservice-parent/microservice-es/src/main/java/com/jack/service/Impl/EsServiceImpl.java
+++ microservice-parent/microservice-es/src/main/java/com/jack/service/Impl/EsServiceImpl.java
@@ -19,7 +19,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -272,5 +271,39 @@ public class EsServiceImpl implements EsService {
             }
         }
         return listNode;
+    }
+
+    /**
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     *
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     *
+     * 注意：给定 n 是一个正整数。
+     *
+     * 示例 1：
+     *
+     * 输入： 2
+     * 输出： 2
+     * 解释： 有两种方法可以爬到楼顶。
+     * 1.  1 阶 + 1 阶
+     * 2.  2 阶
+     * 示例 2：
+     *
+     * 输入： 3
+     * 输出： 3
+     * 解释： 有三种方法可以爬到楼顶。
+     * 1.  1 阶 + 1 阶 + 1 阶
+     * 2.  1 阶 + 2 阶
+     * 3.  2 阶 + 1 阶
+     * 动态条件 计算爬楼梯的方式
+     */
+    public static int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
