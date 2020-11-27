0c57885c0c736c88efba0e83cf38e7a76c307bed
liyjjie <liyongjie@tinsecret.com>
2020-11-27 14:49:05 +0800

    题目

--- microservice-parent/microservice-es/src/main/java/com/jack/service/Impl/EsServiceImpl.java
+++ microservice-parent/microservice-es/src/main/java/com/jack/service/Impl/EsServiceImpl.java
@@ -21,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sun.rmi.log.LogInputStream;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -363,5 +364,36 @@ public class EsServiceImpl implements EsService {
             x = x / 10;
         }
         return y;
+    }
+    /**
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     * 你可以假设数组中无重复元素。
+     * 示例 1:
+     * 输入: [1,3,5,6], 5
+     * 输出: 2
+     * 示例 2:
+     * 输入: [1,3,5,6], 2
+     * 输出: 1
+     * 示例 3:
+     * 输入: [1,3,5,6], 7
+     * 输出: 4
+     * 示例 4:
+     * 输入: [1,3,5,6], 0
+     * 输出: 0
+     * 使用二分法 进行计算
+     */
+   public static int searchInsert(int[] nums,int target){
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
 }
