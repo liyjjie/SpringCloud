8ee2c7ae930ffa6beb608417b0596cca7c919919
liyjjie <liyongjie@tinsecret.com>
2020-12-01 11:40:06 +0800

    修改

--- microservice-parent/microservice-es/src/main/java/com/jack/service/Impl/EsServiceImpl.java
+++ microservice-parent/microservice-es/src/main/java/com/jack/service/Impl/EsServiceImpl.java
@@ -18,7 +18,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sun.rmi.log.LogInputStream;
 
 import java.util.*;
 import java.util.stream.Collectors;
