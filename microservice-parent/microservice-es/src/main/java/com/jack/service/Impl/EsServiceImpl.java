2513992f2fa63878af8246c039704743c9d00bf4
liyjjie <liyongjie@tinsecret.com>
2020-12-01 13:37:52 +0800

    对链表相邻的两个数进行判断

--- microservice-parent/microservice-es/src/main/java/com/jack/service/Impl/EsServiceImpl.java
+++ microservice-parent/microservice-es/src/main/java/com/jack/service/Impl/EsServiceImpl.java
@@ -482,4 +482,57 @@ public class EsServiceImpl implements EsService {
         }
         return result;
     }
+
+    /**
+     * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+     * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+     * 示例 1：
+     * 输入：head = [1,2,3,4]
+     * 输出：[2,1,4,3]
+     * 示例 2：
+     * 输入：head = []
+     * 输出：[]
+     * 示例 3：
+     * 输入：head = [1]
+     * 输出：[1]
+     */
+    public static ListNode swapPairs(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> resultList = new ArrayList<>();
+        while (true) {
+            if (head != null) {
+                list.add(head.val);
+            } else {
+                break;
+            }
+            head = head.next;
+        }
+        if (list.size() % 2 == 0) {
+            for (int i = 0; i <= list.size() - 1; i++) {
+                if (i % 2 == 0) {
+                    resultList.add(i, list.get(i + 1));
+                } else {
+                    resultList.add(i, list.get(i - 1));
+                }
+            }
+        } else {
+            for (int i = 0; i <= list.size() - 2; i++) {
+                if (i % 2 == 0) {
+                    resultList.add(i, list.get(i + 1));
+                } else {
+                    resultList.add(i, list.get(i - 1));
+                }
+            }
+            resultList.add(list.get(list.size() - 1));
+        }
+        ListNode listNode = null;
+        for (int i = resultList.size() - 1; i >= 0; i--) {
+            if (i == resultList.size() - 1) {
+                listNode = new ListNode(resultList.get(i));
+            } else {
+                listNode = new ListNode(resultList.get(i), listNode);
+            }
+        }
+        return listNode;
+    }
 }
